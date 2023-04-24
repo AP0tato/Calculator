9e3528f7825682cb3595251a5d5735bd7fdc7a76
AP0tato <aliabid0207@gmail.com>
2023-04-24 19:56:33 -0400

    commit

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,11 @@
-import tkinter as tk
+from tkinter import *
+import os
 
-tk.Tk()
-tk.mainloop()
+main = Tk()
 
+os.system("cd ../Calculator/ && g++ Main.cpp BinaryTree.cpp Parser.cpp -o ../src/Main")
+
+button = Button(main, text="Press")
+button.pack()
+
+main.mainloop()
