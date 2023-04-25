4d3ebfe3b545201c3f15510a34bbeac9d6e44693
AP0tato <aliabid0207@gmail.com>
2023-04-25 16:16:50 -0400

    commit

--- src/Main.py
+++ src/Main.py
@@ -5,7 +5,15 @@ main = Tk()
 
 os.system("cd ../Calculator/ && g++ Main.cpp BinaryTree.cpp Parser.cpp -o ../src/Main")
 
-button = Button(main, text="Press")
-button.pack()
+numButtons = [ Button(main, text=i) for i in range(10) ]
+
+for i in range(10):
+    numButtons[i].pack()
+
+functions = ['=', '-', '+', '*', '/', ".", "%"]
+funcButtons = [ Button(main, text=x) for x in functions ]
+
+for i in range(len(funcButtons)):
+    funcButtons[i].pack()
 
 main.mainloop()
